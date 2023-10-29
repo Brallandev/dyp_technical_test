7d8c5fb6c81912e438ad55ed32b587d0c542f356
brallandevelop <brallandevelop@gmail.com>
2023-10-29 09:12:25 -0500

    Adding comments to DDL

--- 1.Database/SQL/DDL.sql
+++ 1.Database/SQL/DDL.sql
@@ -1,30 +1,35 @@
-/*DDL SQL Staments following relational    */
+/*DDL SQL Statements following relational according test specifications*/
 
+--CREATE DATABASE
+CREATE DATABASE dyp_test;
 
---CREATE DATABASE dyp_test;
-
+--CREATE Departamento TABLE
 CREATE TABLE Departamento (
 	cod_departamento VARCHAR(3) PRIMARY KEY,
 	nom_departamento VARCHAR(60) NOT NULL
 );
 
+--CREATE Ciudad TABLE
 CREATE TABLE Ciudad (
 	cod_ciudad SMALLINT PRIMARY KEY,
 	nom_ciudad VARCHAR(60) NOT NULL,
 	cod_departamento VARCHAR(3) REFERENCES Departamento(cod_departamento)
 );
 
+--CREATE estados_cliente As a enum type thought there are only two valid options A(Activo), I(Inactivo)
 CREATE TYPE estados_cliente AS ENUM('A','I');
 
+--CREATE Cliente TABLE
 CREATE TABLE Cliente (
 	cedula INTEGER PRIMARY KEY,
 	nombre VARCHAR(60) NOT NULL,
 	telefono VARCHAR(20),
 	cod_ciudad SMALLINT REFERENCES Ciudad(cod_ciudad),
-	estado_cliente estados_clientes NOT NULL DEFAULT 'I',
+	estado_cliente estados_cliente NOT NULL DEFAULT 'I',
 	img_cliente VARCHAR(500)
 );
 
+--CREATE Factura TABLE
 CREATE TABLE Factura (
 	nro_factura INTEGER PRIMARY KEY,
 	cedula INTEGER REFERENCES CLiente(cedula),
